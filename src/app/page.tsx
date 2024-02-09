94e77769b9318eb9f8eeb1e4e1bba20b926f5f59
reversegem_7 <118054229+ReverseGem7@users.noreply.github.com>
2024-02-09 22:25:13 +0100

    Next Image instead of Bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,20 @@ export default function Home() {
     <>
       <div className={`${styles.gradient}`}>
         <div style={{ height: "calc(100vh - 13vh)" }} className="lg:mr-14">
-          <div className="h-4/5 lg:bg-[url('/hero.webp')] sm:bg-cover lg:bg-contain bg-no-repeat bg-right-top w-full">
+          <div className="h-4/5 w-full relative hidden lg:block">
+            {
+              <Image
+                src="/hero.webp"
+                fill={true}
+                alt="Your alr"
+                className="object-contain object-right-top"
+              />
+            }
           </div>
-
 
           <div className="h-1/5 flex items-center justify-center w-screen bg-[#2BE94B] text-white text-2xl font-medium text-center leading-normal xl:text-[36px]">
             How can I use Next Image instead of Bg
           </div>
-
         </div>
       </div>
     </>
